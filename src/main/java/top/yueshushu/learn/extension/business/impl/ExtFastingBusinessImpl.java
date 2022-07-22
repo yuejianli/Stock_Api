e1123e7dfb9d80451c99d1f5c7d54bd534a9a498
yjl <qwe@163.com>
2022-07-22 15:52:16 +0800

    bug: 日期添加 明天的处理

--- src/main/java/top/yueshushu/learn/extension/business/impl/ExtFastingBusinessImpl.java
+++ src/main/java/top/yueshushu/learn/extension/business/impl/ExtFastingBusinessImpl.java
@@ -36,7 +36,7 @@ public class ExtFastingBusinessImpl implements ExtFastingBusiness {
         //能得到信息.
         Date now = DateUtil.date();
         ChineseDate chineseDate = new ChineseDate(now);
-
+    
         // 获取当前的月索引
         int month = chineseDate.getMonth();
         int day = chineseDate.getDay();
@@ -45,9 +45,46 @@ public class ExtFastingBusinessImpl implements ExtFastingBusiness {
         ExtFasting extFasting = extFastingService.getByMonthAndDay(month, day, term);
         //转换成 Vo
         ExtFastingVo extFastingVo = extFastingAssembler.entityToVo(extFasting);
-        if (extFastingVo == null) {
-            return null;
+    
+        String nowDayMessage = null;
+        if (extFastingVo != null) {
+            nowDayMessage = covertVoToMessage(extFastingVo, now, 0);
         }
+        Date morring = DateUtil.offsetDay(now, 1);
+        chineseDate = new ChineseDate(morring);
+    
+        // 获取当前的月索引
+        month = chineseDate.getMonth();
+        day = chineseDate.getDay();
+        //是节气
+        term = chineseDate.getTerm();
+        extFasting = extFastingService.getByMonthAndDay(month, day, term);
+        //转换成 Vo
+        extFastingVo = extFastingAssembler.entityToVo(extFasting);
+    
+        String morringDayMessage = null;
+        if (extFastingVo != null) {
+            morringDayMessage = covertVoToMessage(extFastingVo, morring, 1);
+        }
+        String result = "";
+        String line = System.lineSeparator();
+        if (StringUtils.hasText(nowDayMessage)) {
+            result = result.concat(nowDayMessage).concat(line);
+        }
+        if (StringUtils.hasText(morringDayMessage)) {
+            result = result.concat(morringDayMessage).concat(line);
+        }
+        return result;
+    }
+    
+    /**
+     * 将对象转换成微信要发送的消息
+     *
+     * @param extFastingVo 禁期对象
+     * @return 将对象转换成微信要发送的消息
+     */
+    private String covertVoToMessage(ExtFastingVo extFastingVo, Date date, Integer index) {
+        
         if (StringUtils.isEmpty(extFastingVo.getFastingReason())) {
             extFastingVo.setFastingReasonList(Collections.emptyList());
         } else {
@@ -58,22 +95,12 @@ public class ExtFastingBusinessImpl implements ExtFastingBusiness {
         } else {
             extFastingVo.setDamageList(Arrays.asList(extFastingVo.getDamage().split("\\,")));
         }
-        return covertVoToMessage(extFastingVo);
-    }
-
-    /**
-     * 将对象转换成微信要发送的消息
-     *
-     * @param extFastingVo 禁期对象
-     * @return 将对象转换成微信要发送的消息
-     */
-    private String covertVoToMessage(ExtFastingVo extFastingVo) {
-        Date now = DateUtil.date();
-        ChineseDate chineseDate = new ChineseDate(now);
+        String dayMessage = index == 0 ? "今天" : "明天";
+        ChineseDate chineseDate = new ChineseDate(date);
         StringBuilder stringBuilder = new StringBuilder();
         String line = System.lineSeparator();
         if (extFastingVo.getType() == 1) {
-            stringBuilder.append("今天是农历").append(chineseDate.getChineseMonth())
+            stringBuilder.append(dayMessage + "是农历").append(chineseDate.getChineseMonth())
                     .append(" ").append(chineseDate.getChineseDay()).append(line);
             if (!CollectionUtils.isEmpty(extFastingVo.getFastingReasonList())) {
                 //不为空，往下继续进行.
@@ -87,12 +114,11 @@ public class ExtFastingBusinessImpl implements ExtFastingBusiness {
                     stringBuilder.append("戒由").append(i).append(reason).append("  ")
                             .append(" 犯者报应：").append(damage)
                             .append(line);
-
+                    
                 }
             }
-
         } else {
-            stringBuilder.append("今天是").append(extFastingVo.getJieQi()).append(line);
+            stringBuilder.append(dayMessage + "是").append(extFastingVo.getJieQi()).append(line);
             stringBuilder.append("注意:").append(extFastingVo.getNotes());
         }
         stringBuilder.append(line).append("请注意洁身自好");
